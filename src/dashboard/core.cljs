cf743490fac9cfcd1c5aef86d60c39a9f7bd6400
Josef Erben <josef@erben.io>
2017-10-19 12:51:38 +0200

    finish mock data, use multimeth to render correct chart

--- src/dashboard/core.cljs
+++ src/dashboard/core.cljs
@@ -1,23 +1,38 @@
 (ns dashboard.core
   (:require
-   [dashboard.grid :as grid]
+   [dashboard.grid :as grid :refer [main]]
    [stylefy.core :as stylefy]
    [reagent.core :as reagent :refer [atom]]))
 
 (enable-console-print!)
 
-(defonce app-state (atom {:state
-                             [{:commits {:data
-                                          {:time (vec (range 10))
-                                           :commits [1 2 3 5 8 5 2 12 18 3]}}
-                               :type :line
-                               :pos [0 0]}
-                              {:pull-requests {:data
-                                               {:time (vec (range 10))
-                                                :pull-requests [0 0 1 2 0 2 1 1 2 3]
-                                                :bugs [0 5 3 3 0 2 8 7 5 3]}}
-                               :type :line
-                               :pos [2 2]}]}))
+(defonce app-state (atom {:charts
+                          [{:name :commits
+                            :data {:time (vec (range 10))
+                                   :commits (vec (take 10 (iterate #(+ (rand-int 3) %) 1)))}
+                            :type :line
+                            :pos [0 0]}
+                           {:name :pull-requests
+                            :data
+                            {:time (vec (range 15))
+                             :pull-requests [0 0 1 2 0 2 1 1 2 3 0 1 4 2 4]
+                             :bugs [0 5 3 3 0 2 8 7 5 3 2 5 2 2 5]}
+                            :type :line
+                            :pos [0 2]}
+                           {:name :sprint-progress
+                            :data
+                            {:accepted [0.1 0.1 0.3 0.4 0.5 0.9 1.0 1.0 1.0 1.0]
+                             :in-progress [0.0 0.0 0.0 0.1 0.2 0.3 0.3 0.5 0.5 0.8 0.8 0.8 0.9 1.0]
+                             :done [0.0 0.0 0.0 0.0 0.0 0.0 0.1 0.2 0.3 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0]}
+                            
+                            :type :area
+                            :post [1 0]}
+                           {:name :incidents
+                            :data
+                            {:incidents [0 10 123 23 1 0 12 30 23 45 8 1 74]
+                             :traffic [1 2 40 4 0 0 2 8 8 23 10 23 50]}
+                            :type :scatter
+                            :pos [1 2]}]}))
 
 (defn container []
   "Injects app-state into dashboard, enables re-render"
--- src/dashboard/grid.cljs
+++ src/dashboard/grid.cljs
@@ -3,7 +3,26 @@
    [dashboard.styles.core :as styles]
    [stylefy.core :as stylefy :refer [use-style]]))
 
+(defmulti chart (fn [chart] (:type chart)))
+
+(defmethod chart :line
+  [chart] 
+  [:h2 "Line chart"])
+
+(defmethod chart :area
+  [chart] 
+  [:h2 "Area chart"])
+
+(defmethod chart :scatter
+  [chart] 
+  [:h2 "Scatter chart"])
+  
+(defn- render-charts
+  [widgets]
+  (map chart widgets))
+
 (defn main
   [{:keys [width height state]}]
   [:div.grid (use-style styles/grid-style)
-   [:h1 (:text state)]])
+   (render-charts (state :charts))
+   [:h1 "test"]])
