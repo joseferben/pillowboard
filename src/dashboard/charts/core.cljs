fcfc955dd856247d9d81d819bede26c5c47ec69f
Josef Erben <josef@erben.io>
2017-10-23 11:09:58 +0200

    Generify lines

--- src/dashboard/charts/core.cljs
+++ src/dashboard/charts/core.cljs
@@ -12,23 +12,34 @@
 (def legend js/Recharts.Legend)
 
 (defn- transform [data]
-  (prn (data :data))
   (data :data))
 
-(defn- transform-new
-  ([data]
-   )
-  ([data result]
-   ))
+(defn line-wrapper [key]
+  [:> line {:type "monotone" "dataKey" key}])
+  
+(defn filter-time [keys]
+  (filter #(not= :time %) keys))
+
+(defn- lines [data]
+  (-> data
+      (transform)
+      (first)
+      (keys)
+      (filter-time)
+      (#(map line-wrapper %))
+      (vec)))
 
 (defn- line-chart-comp
   [data]
   [:div
-   [:> line-chart {:width 350 :height 200 :margin {:top 0 :right 0 :bottom 0 :left -40}
+   (into [] (concat
+    [:> line-chart {:width 350 :height 200 :margin {:top 0 :right 0 :bottom 0 :left -40}
                    :data (transform data)}
     [:> x-axis {:data-key :time}]
     [:> y-axis]
     [:> cartesian-grid {"strokeDasharray" "3 3"}]
     [:> tooltip]
-    [:> legend]
-    [:> line {:type "monotone" "dataKey" :commits}]]])
+    [:> legend]]
+    (lines data)
+    ))])
+
