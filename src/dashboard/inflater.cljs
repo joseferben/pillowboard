1e6e104ddacc62a02bfaf2d54e9f84db0356b5f2
Josef Erben <josef@erben.io>
2017-10-29 22:14:32 +0100

    Remove unnecessary brackets

--- src/dashboard/inflater.cljs
+++ src/dashboard/inflater.cljs
@@ -1,27 +1,27 @@
-(ns dashboard.inflator)
+(ns dashboard.inflater)
 
 (def before {:content {:commits {:data [[0 1 2 3 4 5 6 7 8 9] [0 1 1 2 0 2 3 1 4 2]]
                      :meta {:labels [:time :commit]}}
            :sprint {:data [[0.1 0.2 0.3 0.5 0.6 0.6 0.6 0.8 0.9 1.0 1.0 1.0]
                            [0.0 0.0 0.0 0.2 0.2 0.3 0.3 0.4 0.5 0.9 0.9 1.0]
                            [0.0 0.0 0.0 0.0 0.1 0.4 0.3 0.5 0.6 0.9 1.0 1.0]]
-                    :meta {:labels [:accepted :in-progress :donex]}}
+                    :meta {:labels [:accepted :in-progress :done]}}
            :stability {:data [[0 1 2 1 2 4 8 9 8 12 5] [0 1 12 4 2 4 8 14 22 50 49]]
                        :meta {:labels [:incidents :traffic]}}
            :pull-requests {:data [[0 1 2 3 4 5 6 7 8 9]
                                   [0 1 0 0 2 2 3 0 2 1]
                                   [0 1 1 2 2 3 2 1 2 4]]
                            :meta {:labels [:time :pull-requests :bugs]}}}
- :config {:commits [{:type "Line"}]
-          :sprint [{:type "Area"}]
-          :stability [{:type "Scatter"}]
-          :pull-requests [{:type "Line"}]}})
+ :config {:commits {:type "Line"}
+          :sprint {:type "Area"}
+          :stability {:type "Scatter"}
+          :pull-requests {:type "Line"}}})
 
 (defn- get-keys [data]
   (filter #(not= % :board) (keys data)))
 
 (defn- add-default-data
-  [k item]
+  [item k]
   (assoc item :data (or (item :data) k)))
 
 (defn- extract-x-axis
@@ -32,14 +32,15 @@
     (first labels))))
   
 (defn- add-default-axis
-  [data k item]
-  (assoc item :x-axis (or (item :x-axis) (extract-x-axis item data))))
+  [data item k]
+  (if (= (item :type) "Area")
+    item
+    (assoc item :x-axis (or (item :x-axis) (extract-x-axis item data)))))
 
 (defn- add-default
   [config f]
-  (reduce (fn [acc k]
-            (assoc acc k
-                   (vec (map (partial f k) (get config k))))) {}
+  (reduce (fn [acc k] (assoc acc k (f (get config k) k)))
+          {}
           (get-keys config)))
 
 (defn- add-default-config
---
+++ src/dashboard/transformer.cljs
@@ -0,0 +1,79 @@
+(ns dashboard.transformer)
+
+(def after {:charts
+                          [{:name :commits
+                            :data
+                            [{:time 0 :commits 1}
+                             {:time 1 :commits 3}
+                             {:time 2 :commits 2}
+                             {:time 3 :commits 7}
+                             {:time 4 :commits 3}
+                             {:time 5 :commits 2}
+                             {:time 6 :commits 6}
+                             {:time 7 :commits 5}
+                             {:time 8 :commits 2}
+                             {:time 9 :commits 4}]
+                            :type :line
+                            :pos [0 0]}
+                           {:name :pull-requests
+                            :data
+                            [{:time 0 :pull-requests 0 :bugs 0}
+                             {:time 1 :pull-requests 2 :bugs 1}
+                             {:time 2 :pull-requests 3 :bugs 2}
+                             {:time 3 :pull-requests 1 :bugs 1}
+                             {:time 4 :pull-requests 2 :bugs 6}
+                             {:time 5 :pull-requests 0 :bugs 7}
+                             {:time 6 :pull-requests 2 :bugs 3}
+                             {:time 7 :pull-requests 1 :bugs 2}
+                             {:time 8 :pull-requests 8 :bugs 2}]
+                            :type :line
+                            :pos [0 2]}
+                           {:name :sprint-progress
+                            :data
+                            [{:accepted 0.1 :in-progress 0.0 :done 0.0}
+                             {:accepted 0.2 :in-progress 0.0 :done 0.0}
+                             {:accepted 0.2 :in-progress 0.0 :done 0.0}
+                             {:accepted 0.3 :in-progress 0.2 :done 0.0}
+                             {:accepted 0.4 :in-progress 0.2 :done 0.1}
+                             {:accepted 0.5 :in-progress 0.3 :done 0.1}
+                             {:accepted 0.5 :in-progress 0.3 :done 0.2}
+                             {:accepted 0.5 :in-progress 0.8 :done 0.2}
+                             {:accepted 0.8 :in-progress 0.8 :done 0.3}
+                             {:accepted 0.8 :in-progress 0.9 :done 0.8}
+                             {:accepted 0.9 :in-progress 0.9 :done 0.9}
+                             {:accepted 1.0 :in-progress 0.9 :done 0.8}
+                             {:accepted 1.0 :in-progress 1.0 :done 0.8}
+                             {:accepted 1.0 :in-progress 1.0 :done 0.9}
+                             {:accepted 1.0 :in-progress 1.0 :done 1.0}]
+                            :type :area
+                            :post [1 0]}
+                           {:name :incidents
+                            :data
+                            [{:incidents 0 :traffic 1}
+                             {:incidents 2 :traffic 3}
+                             {:incidents 1 :traffic 0}
+                             {:incidents 4 :traffic 1}
+                             {:incidents 8 :traffic 3}
+                             {:incidents 23 :traffic 12}
+                             {:incidents 12 :traffic 48}
+                             {:incidents 8 :traffic 23}
+                             {:incidents 21 :traffic 79}
+                             {:incidents 18 :traffic 97}
+                             {:incidents 9 :traffic 123}]
+                            :type :scatter
+                            :pos [1 2]}]})
+
+(defn key->chart
+  [data key]
+  {:name key
+   :data (get-in data [:content key])
+   :type (get-in data [:config key])})
+  
+
+(defn extract-charts
+  [data]
+  (map (partial key->chart data) (keys (data :config))))
+
+(defn transform
+  [data]
+  {:charts (extract-charts data)})
