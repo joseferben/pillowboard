8c2a0532a7e810aba4db7e8ba9b0c1d51a840587
Josef Erben <josef@erben.io>
2017-11-02 15:16:42 +0100

    Run tests by comparing .edn files

--- src/clj/dashboard/inflater.clj
+++ src/clj/dashboard/inflater.clj
@@ -1,35 +1,5 @@
 (ns dashboard.inflater)
 
-;; TODO
-;; move to backend, write tests, replace default function calls
-;; with default schema, merging and reolver functions called at
-;; the right places
-
-(def after {:content {:commits {:data [[0 1 2 3 4 5 6 7 8 9] [0 1 1 2 0 2 3 1 4 2]]
-                     :meta {:labels [:time :commit]}}
-           :sprint {:data [[0.1 0.2 0.3 0.5 0.6 0.6 0.6 0.8 0.9 1.0 1.0 1.0]
-                           [0.0 0.0 0.0 0.2 0.2 0.3 0.3 0.4 0.5 0.9 0.9 1.0]
-                           [0.0 0.0 0.0 0.0 0.1 0.4 0.3 0.5 0.6 0.9 1.0 1.0]]
-                    :meta {:labels [:accepted :in-progress :donex]}}
-           :stability {:data [[0 1 2 1 2 4 8 9 8 12 5] [0 1 12 4 2 4 8 14 22 50 49]]
-                       :meta {:labels [:incidents :traffic]}}
-           :pull-requests {:data [[0 1 2 3 4 5 6 7 8 9]
-                                  [0 1 0 0 2 2 3 0 2 1]
-                                  [0 1 1 2 2 3 2 1 2 4]]
-                           :meta {:labels [:time :pull-requests :bugs]}}}
- :config {:commits [{:type "Line"
-                     :data :commits
-                     :x-axis :time}]
-          :sprint [{:type "Area"
-                    :data :sprint}]
-          :stability [{:type "Scatter"
-                       :data :stability
-                       :x-axis :time}]
-          :pull-requests [{:type "Line"
-                           :data :pull-requests
-                           :x-axis :time}]
-          :board {:width 3}}})
-
 (def static-default-config {:board {:width 3}})
   
 (defn- get-keys [data]
--- test/clj/dashboard/inflater_test.clj
+++ test/clj/dashboard/inflater_test.clj
@@ -1,49 +1,7 @@
 (ns clj.dashboard.inflater-test
   (:require [dashboard.inflater :as sut]
+            [clojure.edn :as edn]
             [clojure.test :refer [deftest testing is]]))
-
-(def before {:content {:commits {:data [[0 1 2 3 4 5 6 7 8 9] [0 1 1 2 0 2 3 1 4 2]]
-                                 :meta {:labels [:time :commit]}}
-                       :sprint {:data [[0.1 0.2 0.3 0.5 0.6 0.6 0.6 0.8 0.9 1.0 1.0 1.0]
-                                       [0.0 0.0 0.0 0.2 0.2 0.3 0.3 0.4 0.5 0.9 0.9 1.0]
-                                       [0.0 0.0 0.0 0.0 0.1 0.4 0.3 0.5 0.6 0.9 1.0 1.0]]
-                                :meta {:labels [:accepted :in-progress :donex]}}
-                       :stability {:data [[0 1 2 1 2 4 8 9 8 12 5] [0 1 12 4 2 4 8 14 22 50 49]]
-                                   :meta {:labels [:incidents :traffic]}}
-                       :pull-requests {:data [[0 1 2 3 4 5 6 7 8 9]
-                                              [0 1 0 0 2 2 3 0 2 1]
-                                              [0 1 1 2 2 3 2 1 2 4]]
-                                       :meta {:labels [:time :pull-requests :bugs]}}}
-             :config {:commits {:type :line}
-                      :sprint {:type :area}
-                      :stability {:type :scatter}
-                      :pull-requests {:type :line}}})
-
-(def after {:content {:commits {:data [[0 1 2 3 4 5 6 7 8 9] [0 1 1 2 0 2 3 1 4 2]]
-                                :meta {:labels [:time :commit]}}
-                      :sprint {:data [[0.1 0.2 0.3 0.5 0.6 0.6 0.6 0.8 0.9 1.0 1.0 1.0]
-                                      [0.0 0.0 0.0 0.2 0.2 0.3 0.3 0.4 0.5 0.9 0.9 1.0]
-                                      [0.0 0.0 0.0 0.0 0.1 0.4 0.3 0.5 0.6 0.9 1.0 1.0]]
-                               :meta {:labels [:accepted :in-progress :donex]}}
-                      :stability {:data [[0 1 2 1 2 4 8 9 8 12 5] [0 1 12 4 2 4 8 14 22 50 49]]
-                                  :meta {:labels [:incidents :traffic]}}
-                      :pull-requests {:data [[0 1 2 3 4 5 6 7 8 9]
-                                             [0 1 0 0 2 2 3 0 2 1]
-                                             [0 1 1 2 2 3 2 1 2 4]]
-                                      :meta {:labels [:time :pull-requests :bugs]}}}
-            :config {:commits {:type :line 
-                                :data :commits
-                                :x-axis :time}
-                     :sprint {:type :area 
-                               :data :sprint}
-                     :stability {:type :scatter
-                                  :data :stability
-                                  :x-axis :incidents}
-                     :pull-requests {:type :line
-                                      :data :pull-requests
-                                      :x-axis :time}
-                     :board {:width 3}}})
-
 
 (testing "inflater"
   (deftest merge-static-defaults
@@ -54,6 +12,6 @@
       (is (= expected (fun target defaults)))))
 
   (deftest inflate
-    (let [actual (sut/inflate before)
-        expected after]
+    (let [actual (sut/inflate (edn/read-string (slurp "resources/sample.edn")))
+          expected (edn/read-string (slurp "resources/sample-inflated.edn"))]
     (is (= actual expected)))))
