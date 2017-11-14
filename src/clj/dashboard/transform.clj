cb72897a160d7508070a889ed4c1fb0180d291ce
Josef Erben <josef@erben.io>
2017-11-14 15:52:17 +0100

    Implement transformation logic

--- src/clj/dashboard/transform.clj
+++ src/clj/dashboard/transform.clj
@@ -1,3 +1,27 @@
 (ns dashboard.transform)
 
-(defn transform [configured])
+(defn- transform-meta [{:keys [metrics]}]
+  (->> metrics
+       (map #(dissoc % :data))
+       vec))
+
+(defn- metrics->matrix [metrics]
+  (->> metrics
+      (map #(get % :data))
+      vec
+      (apply mapv vector)))
+
+(defn- transform-metrics [{:keys [metrics]}]
+  (let [names (map #(get % :name) metrics)]
+    (->> metrics
+         metrics->matrix
+         (map (fn [row] (zipmap names row))))))
+
+(defn- transform-chart [chart]
+  (-> chart
+      (assoc :data (transform-metrics chart))
+      (assoc :meta (transform-meta chart))
+      (dissoc :metrics)))
+
+(defn transform [configured]
+  (update configured :charts (fn [charts] (map transform-chart charts))))
