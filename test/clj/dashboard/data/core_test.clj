b499789a1eeb7d41a0f21f01664a602a81b63875
Josef Erben <josef@erben.io>
2017-11-15 11:56:38 +0100

    Add test to check different lengths

--- test/clj/dashboard/data/core_test.clj
+++ test/clj/dashboard/data/core_test.clj
@@ -8,4 +8,9 @@
     (let [tupel1 [[1 2 3 4] [2 3 1 2]]
           tupel2 [[1 2 5 6] [2 1 0 0]]
           expected [[1 2 3 4 5 6] [2 3 1 2 2 2] [2 1 1 1 0 0]]]
+      (is (= expected (sut/full-join-last-value tupel1 tupel2)))))
+  (deftest full-join-last-value-strategy
+    (let [tupel1 [[1 3] [2 3]]
+          tupel2 [[1 2 5 6] [2 1 0 0]]
+          expected [[1 2 3 5 6] [2 2 3 3 3] [2 1 1 0 0]]]
       (is (= expected (sut/full-join-last-value tupel1 tupel2))))))
