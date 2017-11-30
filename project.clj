5fc1a505ce3189256fd142829e7641b777851d75
Josef Erben <josef@erben.io>
2017-11-30 22:04:50 +0100

    Add hacky postgres migrations

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dashboard "0.0.4-SNAPSHOT"
+(defproject dashboard "0.1.0-SNAPSHOT"
   :description "Zero config dashboard that makes sense of your data."
   :url "http://www.200ok.ch"
   :license {:name "Eclipse Public License"
