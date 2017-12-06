53fa2f46e4119fabcaf8794f5ab82c5a16af8442
Josef Erben <josef@erben.io>
2017-12-06 14:22:11 +0100

    Implement event insertion and dashboard retrieval.

--- src/clj/dashboard/db.clj
+++ src/clj/dashboard/db.clj
@@ -11,6 +11,9 @@
 
 (def db (or (env :database-url) "http://localhost:5984/db"))
 
+(def doc-views {:dashboard {:view "dashboards-view"}
+                :user {:view "users-view"}})
+
 (defn- p-get
   "Returns the body of a response to a GET request to `url` as clojure map."
   [url]
@@ -24,15 +27,25 @@
   [url body]
   (client/put url {:body (generate-string body)}))
 
+(defn- map->query [query]
+  (str "?" (clojure.string/join "&" (map #(str (first %) "=" (second %)) query))))
+
 (defn- get-doc [doc-id]
   (p-get (str db "/" doc-id)))
+
+(defn- get-view [{view :view} & [query]]
+  (p-get (str db "/_design/doc/_view/" view (map->query query))))
+
+(defn- put-doc!
+  "PUTs a document with `doc-id` with `init`ial values."
+  [doc-id & [init]]
+  (p-put! (str db "/" doc-id) (or init {})))
 
 (defn- update-doc!
   "Updates a document with id `doc-id` by applying a function
   `f` with arguments `args` to the old version of that document."
   [doc-id f & args]
   (let [old (get-doc doc-id)]
-    (prn args)
     (p-put! (str db "/" doc-id)
             (merge (apply f old args) {"_rev" (get old "_rev")}))))
 
@@ -40,51 +53,24 @@
   "Retrieves a list of all stored events for the dashboard with `id`.
   The list may contain events of different types."
   [id]
-  (client/get db)
-  (concat (events/events-timeseries-all db {:id id})
-          (events/events-gauge-all db {:id id})
-          (events/events-tuple-all db {:id id})))
+  (get (get-doc id) "events" []))
 
 (defn- event-meta-insert!
   "Stores meta data of an event."
   [event-id {:keys [mode]}]
   (events/event-meta-insert db {:event_id event-id :mode mode}))
 
-(defmulti event-insert!
-  "Stores an `event` of any type with dashboard `id`."
-  event-type)
-
-(defmethod event-insert! :timeseries [{:keys [name time value mode]} id]
-  (let [event-id (events/event-timeseries-insert db {:dashboard_id id
-                                                     :name name
-                                                     :time time
-                                                     :value value})]
-    (when (some? mode)
-      (event-meta-insert! event-id {:mode mode}))))
-
-(defmethod event-insert! :gauge [{:keys [name value mode]} id]
-  (let [event-id (events/event-gauge-insert db {:dashboard_id id
-                                                :name name
-                                                :value value})]
-    (when (some? mode)
-      (event-meta-insert! event-id {:mode mode}))))
-
-(defmethod event-insert! :tuple [{:keys [name1 value1 name2 value2 mode]} id]
-  (let [event-id (events/event-tuple-insert db {:dashboard_id id
-                                                :name1 name1
-                                                :value1 value1
-                                                :name2 name2
-                                                :value2 value2})]
-    (when (some? mode)
-      (event-meta-insert! event-id {:mode mode}))))
+(defn event-insert!
+  [event board-id]
+  (update-doc! board-id update "events" conj event))
 
 (defn dashboards-all
-  "Retrieves a list of dashboard for a user with `user-id`.
+  "Retrieves a list of dashboard id name paris for a user with `user-id`.
   Without argument all dashboards are returned."
   ([user-id]
-   (dashboards/dashboards-of-user db {:user_id user-id}))
+   (get-view (doc-views :dashboard) {"key" user-id}))
   ([]
-   (dashboards/dashboards-all db)))
+   (get-view (doc-views :dashboard))))
 
 (defn dashboard-insert!
   "Stores a dashboard for a given user with `user-id`."
