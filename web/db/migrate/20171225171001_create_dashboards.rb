be05ba439f1816bd8ea8138e42ceb2efdedac268
Josef Erben <josef@erben.io>
2017-12-26 17:20:34 +0100

    Replace sqlite with postgres through docker setup

--- web/db/migrate/20171225171001_create_dashboards.rb
+++ web/db/migrate/20171225171001_create_dashboards.rb
@@ -1,6 +1,6 @@
 class CreateDashboards < ActiveRecord::Migration[5.1]
   def change
-    create_table :dashboards, id: :uuid do |t|
+    create_table :dashboards do |t|
       t.string :name
       t.references :user, foreign_key: true
 
--- web/db/schema.rb
+++ web/db/schema.rb
@@ -12,8 +12,17 @@
 
 ActiveRecord::Schema.define(version: 20171225171001) do
 
-# Could not dump table "dashboards" because of following StandardError
-#   Unknown type 'uuid' for column 'id'
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "dashboards", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_dashboards_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_dashboards_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,4 +32,5 @@ ActiveRecord::Schema.define(version: 20171225171001) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "dashboards", "users"
 end
