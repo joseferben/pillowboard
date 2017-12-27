e0db1d996908e567d3781f343992577431143a9b
Josef Erben <josef@erben.io>
2017-12-27 16:50:46 +0100

    Layout admin page and add positive integration test

--- web/test/integration/users_signup_test.rb
+++ web/test/integration/users_signup_test.rb
@@ -11,4 +11,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
 end
