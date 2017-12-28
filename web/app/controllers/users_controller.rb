903727365af15c272dc993991cfc2c136173c016
Josef Erben <josef@erben.io>
2017-12-28 15:54:43 +0100

    Add integration test for logout

--- web/app/controllers/users_controller.rb
+++ web/app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Dashboard!"
       redirect_to @user
     else
--- web/app/helpers/sessions_helper.rb
+++ web/app/helpers/sessions_helper.rb
@@ -12,4 +12,9 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
 end
