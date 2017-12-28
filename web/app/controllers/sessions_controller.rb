dfd1f808992c8f3bb387a931b538109d0451763e
Josef Erben <josef@erben.io>
2017-12-28 17:35:31 +0100

    Implement cookies based session persistence

--- web/app/controllers/sessions_controller.rb
+++ web/app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      remember user
+      remember user if params[:session][:remember] == '1'
       redirect_to user
     else
       flash[:danger] = 'Invalid email/password combination'
