3244a29d8a59b39790c15b236a2768d6aa15f4fe
Josef Erben <josef@erben.io>
2017-12-28 15:41:36 +0100

    Add integration test for happy login path

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root 'pages#login'
-  get '/login', to: 'pages#login'
+  root 'sessions#new'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
