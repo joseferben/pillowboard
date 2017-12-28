eb6fc6ff8a1336dd61f4e2bb4ab54358a6573b80
Josef Erben <josef@erben.io>
2017-12-28 17:49:19 +0100

    Cover untested lines of cookie based auth with tests

--- web/app/controllers/sessions_controller.rb
+++ web/app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      remember user if params[:session][:remember] == '1'
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash[:danger] = 'Invalid email/password combination'
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 
