53e6fe180c23e1e86cc0cf212fc4951388406d50
Josef Erben <josef@erben.io>
2017-12-29 19:21:29 +0100

    Persist ws connection in ratom

--- board/src/cljs/dashboard/core.cljs
+++ board/src/cljs/dashboard/core.cljs
@@ -1,19 +1,25 @@
 (ns dashboard.core
   (:require [reagent.core :as r]
             [taoensso.timbre :as timbre :refer-macros (tracef debugf infof warnf errorf)]
-            ;;[dashboard.events]
-            ;;[dashboard.subs]
+            [dashboard.db :refer [db]]
+            [dashboard.ws :refer [handle-event]]
+            [dashboard.actions :refer [init-db! init-ws!]]
             [dashboard.views :as views]
             [devtools.core :as devtools]))
 
 (devtools/install!)
 (enable-console-print!)
-
 (timbre/set-level! :debug)
 
 (infof "ClojureScript appears to have loaded correctly.")
 
+(defn init!
+  []
+  (init-db!)
+  (init-ws! handle-event)
+  (r/render [views/page (@db :board)]
+            (.getElementById js/document "app")))
+
 (defn ^:export main
   []
-  (r/render [views/page nil]
-                  (.getElementById js/document "app")))
+  (init!))
