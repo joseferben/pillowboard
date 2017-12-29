736d3042a1450dd353a778c86d072b2182f9da6e
Josef Erben <josef@erben.io>
2017-12-29 16:56:01 +0100

    Implement creation and presentation of available boards

--- web/app/controllers/dashboards_controller.rb
+++ web/app/controllers/dashboards_controller.rb
@@ -1,2 +1,17 @@
 class DashboardsController < ApplicationController
+
+  def create
+    @dashboard = current_user.dashboards.create(dashboard_params)
+    if @dashboard.save
+      redirect_to current_user
+    else
+      flash[:danger] = "Failed to add dashboard"
+    end
+  end
+
+ private
+    def dashboard_params
+      params.require(:dashboard).permit(:name)
+    end
+
 end
