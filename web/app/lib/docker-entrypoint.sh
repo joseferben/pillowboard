51820dfce35b1a6720861964e19a686e84b5239a
Josef Erben <josef@erben.io>
2018-01-01 16:40:56 +0100

    Prepare for less cumbersome prod deployment

--- web/app/lib/docker-entrypoint.sh
+++ web/app/lib/docker-entrypoint.sh
@@ -2,8 +2,8 @@
 # https://stackoverflow.com/a/38732187/1935918
 set -e
 
-if [ -f /app/tmp/pids/server.pid ]; then
-  rm /app/tmp/pids/server.pid
+if [ -f /myapp/tmp/pids/server.pid ]; then
+  rm /myapp/tmp/pids/server.pid
 fi
 
 bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
