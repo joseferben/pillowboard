a05ad812feb3bc33cf27e2c717420d77c08784b6
Josef Erben <josef@erben.io>
2018-03-25 12:08:49 +0200

    Remove all artifacts of rails backend

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -jar target/dashboard-standalone.jar 
--- web/db/migrate/20171229160820_add_uuid_to_dashboards.rb
+++
@@ -1,5 +0,0 @@
-class AddUuidToDashboards < ActiveRecord::Migration[5.1]
-  def change
-    add_column :dashboards, :uuid, :string
-  end
-end
