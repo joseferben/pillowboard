a90cea0098f6a066290f983908a10069d248664d
Josef Erben <josef@erben.io>
2018-08-18 09:47:33 +0200

    Test transform logic

--- board/src/clj/dashboard/pipeline/transform.clj
+++ board/src/clj/dashboard/pipeline/transform.clj
@@ -1,11 +1,10 @@
 (ns dashboard.pipeline.transform)
 
-(defn- sort-metrics [metrics]
+(defn sort-metrics [metrics]
   (sort (fn [a b] (compare (get a "time") (get b "time"))) metrics))
 
 (defn- sort-charts [charts]
-  (map (fn [chart]
-         (update chart :metrics sort-metrics)) charts))
+  (map #(update % :metrics sort-metrics) charts))
 
 (defn transform [configured]
   (update configured :charts sort-charts))
--- board/test/clj/dashboard/pipeline/transform_test.clj
+++ board/test/clj/dashboard/pipeline/transform_test.clj
@@ -3,9 +3,20 @@
              [clojure.edn :as edn]
              [clojure.test :refer [deftest testing is]]))
 
-;; replace with good test, renderer can consume 05-configured data
-(comment (testing "transform"
-  (deftest transform
-   (let [actual (sut/transform (edn/read-string (slurp "resources/05-configured.edn")))
-          expected (edn/read-string (slurp "resources/06-transformed.edn"))]
-     (is (= expected actual))))))
+(deftest transform-board-with-no-charts
+  (let [actual (sut/transform {:charts []})]
+    (is (= {:charts []} actual))))
+
+(deftest transform-fix-invalid-input
+  (is (= {:charts []}
+         (sut/transform {:charts nil})))
+  (is (= {:charts [{:metrics []}]}
+         (sut/transform {:charts [{:metrics nil}]}))))
+
+(deftest sort-metrics
+  (let [input [{:foo 42} {:bar 24}]]
+     (is (= input (sut/sort-metrics input))))
+  (is (= [{"time" 24} {"time" 42}]
+         (sut/sort-metrics [{"time" 42} {"time" 24}])))
+  (is (= [{"time" "abc"} {"time" "bcd"}]
+         (sut/sort-metrics [{"time" "bcd"} {"time" "abc"}]))))
